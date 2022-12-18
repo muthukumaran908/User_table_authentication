727655dd5af1fe52c557bd49f8aba9ceefedb60d
muthukumaran908 <kumaran3853@gmail.com>
2022-12-19 01:07:17 +0530

    card task

---
+++ task_number_of_cards.js
@@ -0,0 +1,9 @@
+var total_cards=52;
+var start_index=7;
+var total_person=15;
+
+var balance_person = total_cards % total_person;
+
+var result=balance_person + start_index -1
+
+console.log("output :",result);
